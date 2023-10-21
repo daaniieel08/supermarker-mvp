7bf13b7f1ae2d4df935b0e44914c11e1a4dbd4ba
Daniel Diaz <daaniieeliitoodiiaaz@gmail.com>
2023-10-21 00:13:57 -0500

    Seguando Guardado

--- SQLQuery1.sql
+++ SQLQuery1.sql
@@ -4,7 +4,7 @@ create table PayMode
  Pay_Mode_Id int identity (100000,1) primary key,
  Pay_Mode_Name nvarchar (50) not null,
  Pay_Mode_Observation nvarchar (50) not null,
-)
+)git log.
 go
 insert into PayMode values('Cash', 'Cash mode')
 insert into PayMode values('Credit card', 'Credit card mode')
